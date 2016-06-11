0ea1c29628e824244f2afbbd2c59ae7352313d62
tejbans <singh.tejbans@gmail.com>
2016-06-11 16:03:53 -0700

    Done.

--- code.js
+++ code.js
@@ -1,1 +1,17 @@
 'use strict';
+
+function favIceCream(name){
+  return "I love " + name ;
+}
+
+function shouting(phrase){
+  return phrase.toUpperCase();
+}
+
+function roundDown(n){
+  return Math.floor(n);
+}
+
+function theTruth(){
+ return true
+}
